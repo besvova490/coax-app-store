6a0f7ad608d57b61704489a28bca9a9053a91b11
besvova490 <besvova490@gmail.com>
2020-12-15 14:56:31 +0200

    add scroll to top

--- src/components/productsList/productsList.js
+++ src/components/productsList/productsList.js
@@ -11,21 +11,26 @@ const ProductsList = ({startIndex, productsBooks, toggleWantedList, addToCartLis
     }
 
     return (
-        <InfiniteScroll className='row items-list'
-                        initialLoad={false}
-                        hasMore={!error}
-                        loader={<Loader key={"loader"}/>}
-                        loadMore={() => {getAllProductsRequestAction({startIndex: startIndex})}}>
-        {
-            productsBooks.map(item => <ProductsItems key={item.id}
-                                             itemBook={item}
-                                             buttonLabel='Add to wanted'
-                                             toggleWantedList={toggleWantedList}
-                                             addToCartList={addToCartList}
-                                             wantedItems={wantedItems}
-                                             isAuth={isAuth}/>)
-        }
-        </InfiniteScroll>
+        <React.Fragment>
+            <InfiniteScroll className='row items-list'
+                            initialLoad={false}
+                            hasMore={!error}
+                            loader={<Loader key={"loader"}/>}
+                            loadMore={() => {getAllProductsRequestAction({startIndex: startIndex})}}>
+            {
+                productsBooks.map(item => <ProductsItems key={item.id}
+                                                 itemBook={item}
+                                                 buttonLabel='Add to wanted'
+                                                 toggleWantedList={toggleWantedList}
+                                                 addToCartList={addToCartList}
+                                                 wantedItems={wantedItems}
+                                                 isAuth={isAuth}/>)
+            }
+            </InfiniteScroll>
+            <div className="arrow-to-top" onClick={() => window.scrollTo(0,0)}>
+                <i className="fa fa-chevron-circle-up" aria-hidden="true"/>
+            </div>
+        </React.Fragment>
     );
 };
 
